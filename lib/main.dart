d4abab437dde7b88a547bcb071444d0fc1c88fc1
ArthurMarianoNeto <thiesto@gmail.com>
2021-03-01 19:02:38 -0300

    #11 Testando acesso ao banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+
+  Firestore.instance.collection('Pedidos').document('#0001').setData(
+      {'preco': 199.99, 'usuário' : 'Arthur Mariano'}
+      );
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+      if(true) {
+        print("Testando Software");
+      }
 
     return MaterialApp(
       title: 'Flutter Demo',
