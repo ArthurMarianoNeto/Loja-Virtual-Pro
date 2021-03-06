fb16dc8e8424ec0246de5bbaaf953b7910b3f12e
ArthurMarianoNeto <thiesto@gmail.com>
2021-03-05 22:57:39 -0300

    #14 Mapeando cada página de acordo com o page_controller

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -2,28 +2,41 @@ import 'package:flutter/material.dart';
 
 class DrawerTile extends StatelessWidget {
 
-  DrawerTile({this.iconData, this.title});
+  const DrawerTile({this.iconData, this.title, this.page});
 
   final IconData iconData;
   final String title;
+  final int page;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Icon(
-          iconData,
-          size: 32,
-          color: Colors.grey[700],
+    return InkWell(
+      onTap: (){
+        debugPrint('Apertei $page');
+      },
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Icon(
+                iconData,
+                size: 32,
+                color: Colors.grey[700],
+              ),
+            ),
+            const SizedBox(width: 32,),
+            Text(
+              title,
+              style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey[700]
+            ),
+           )
+          ],
         ),
-        Text(
-          title,
-          style: TextStyle(
-          fontSize: 16,
-          color: Colors.grey[700]
-        ),
-       )
-      ],
+      ),
     );
   }
 }
